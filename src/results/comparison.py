3f4f1cd93559f3c975cbc274e72433558077dfe5
Bo Zhang <bz16@nyu.edu>
2015-04-01 09:34:05 +0800

    Comparison figs generated. Plot BN subtracts Baseline

--- src/results/comparison.py
+++ src/results/comparison.py
@@ -2,12 +2,12 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 
-with open("nn-vanilla-deep.json") as baseline_file:
+with open("nn-vanilla.json") as baseline_file:
     data=json.load(baseline_file)
 accu_base=np.array(data['accuracy_epoch'])
 loss_base=np.array(data['crossEntropy_epoch'])
 
-with open("nn-batchnorm-deep.json") as bn_file:
+with open("nn-batchnorm.json") as bn_file:
     data=json.load(bn_file)
 accu_bn=np.array(data['accuracy_epoch'])
 loss_bn=np.array(data['crossEntropy_epoch'])
@@ -17,9 +17,9 @@ thres=np.zeros(len(diff))
 plt.figure(1)
 plt.plot(diff,'ro',diff,'b',thres,'r')
 plt.grid(True)
-plt.text(40,0.7,"learningrate:\n BN: 0.2,\n Baseline:0.02")
-plt.xlabel('# of Iterations (Unit {0} batches)'.format(50))
+plt.text(60,0.04,"learningrate:\n BN: 0.2,\n Baseline:0.02")
+plt.xlabel('# of Iterations (Unit: {0} batches)'.format(50))
 plt.ylabel('Accuracy: BN-Baseline')
-plt.savefig('DeepNet_Compare.png')
+plt.savefig('comp-one-hiddenlayer.eps',format='eps',dpi=720)
 plt.show()
 
